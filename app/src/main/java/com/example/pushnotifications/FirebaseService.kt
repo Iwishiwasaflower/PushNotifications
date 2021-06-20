9841e8474532c1c806b6090a9df85618f811910d
DJCoding <Dominik.Jantschar0@hm.edu>
2021-06-20 18:31:07 +0200

    added comments

--- app/src/main/java/com/example/pushnotifications/FirebaseService.kt
+++ app/src/main/java/com/example/pushnotifications/FirebaseService.kt
@@ -17,29 +17,45 @@ import androidx.core.os.bundleOf
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlin.random.Random
-
+//Channels which the App listens to.
+//For better user controlling which notifications he wants to ignore/see
+//For additional Settings like Sound/Receive type.
 private const val CHANNEL_ID = "my_channel"
+private const val CHANNEL_NAME = "Channel_IGNORE"
+
+
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class FirebaseService : FirebaseMessagingService() {
 
+    /**
+     * implements the interface for receiving messages from extern services.
+     * Mainly used as Service is Google Firebase.
+     */
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
+        //The Communication to an Activity works with intent.
+        //Explicit Intent is a messaging object used to request an action from another app component.
         val intent = Intent(this, MainActivity::class.java)
+
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notificationID = Random.nextInt()
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             createNotificationChannel(notificationManager)
         }
-        Log.d("message", message.notification.toString())
-        val asd = message.notification
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
+        Log.d("message", message.notification.toString())
+
+        //That the activity we intent to perform an action is already running and should be now the "new actual activity"
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        //This declares that we the intent is a one time object we wont use it another time.
         val pendingIntent = PendingIntent.getActivity(this,0, intent, FLAG_ONE_SHOT)
 
         // von fb console - works
         if(message.data["title"] == null){
+            //Design of the Notification
             val notification = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle(message.notification?.title.toString())
                 .setContentText(message.notification?.body.toString())
@@ -64,17 +80,20 @@ class FirebaseService : FirebaseMessagingService() {
         }
 
     }
-
+    /**
+     * This Methode creates the Notification channel on which the Notification gets send.
+     * The purpose is, to have more Settings options for the user.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager : NotificationManager){
-        val channelName = "channelName"
+        //How it should be delivered.
         val channel =
-            NotificationChannel(CHANNEL_ID, channelName, IMPORTANCE_HIGH).apply {
+            NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE_HIGH).apply {
                 description = "My channel discription"
                 enableLights(true)
                 lightColor = Color.GREEN
+                enableVibration(true)
             }
             notificationManager.createNotificationChannel(channel)
-
     }
 }
