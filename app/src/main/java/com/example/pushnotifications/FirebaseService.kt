a83af7f8fe9b05d1f460093652cc2ef377bfe072
Felix Schnitzenbaumer <felix.schnitzenbaumer@hm.edu>
2021-06-20 14:03:37 +0200

    Added non working local notification added non working token

--- app/src/main/java/com/example/pushnotifications/FirebaseService.kt
+++ app/src/main/java/com/example/pushnotifications/FirebaseService.kt
@@ -8,6 +8,7 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_ONE_SHOT
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
@@ -19,8 +20,25 @@ import com.google.firebase.messaging.RemoteMessage
 import kotlin.random.Random
 
 private const val CHANNEL_ID = "my_channel"
-@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+
 class FirebaseService : FirebaseMessagingService() {
+
+    companion object{
+        var sharedPref : SharedPreferences? = null
+
+        var token : String?
+        get() {
+                    return sharedPref?.getString("token", "")
+                }
+        set(value){
+            sharedPref?.edit()?.putString("token", value)?.apply()
+        }
+    }
+
+    override fun onNewToken(newToken: String) {
+        super.onNewToken(newToken)
+        token = newToken
+    }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
@@ -32,8 +50,8 @@ class FirebaseService : FirebaseMessagingService() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             createNotificationChannel(notificationManager)
         }
-        Log.d("message", message.notification.toString())
-        val asd = message.notification
+
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
         val pendingIntent = PendingIntent.getActivity(this,0, intent, FLAG_ONE_SHOT)
@@ -50,8 +68,8 @@ class FirebaseService : FirebaseMessagingService() {
 
             notificationManager.notify(notificationID,notification)
         } else {
-            // sich selbst schicken via firebase, does not work - crash
-            Log.d("Message", message.data["message"].toString())
+            // sich selbst schicken via firebase - works
+
             val notification = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle(message.data["title"])
                 .setContentText(message.data["message"])
