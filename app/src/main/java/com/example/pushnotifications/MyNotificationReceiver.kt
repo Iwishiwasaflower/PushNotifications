cf498a1c6a11aa003bc919eb1b562fcafc8aff25
DJCoding <Dominik.Jantschar0@hm.edu>
2021-06-24 14:34:23 +0200

    removed code

--- app/src/main/java/com/example/pushnotifications/MyNotificationReceiver.kt
+++ app/src/main/java/com/example/pushnotifications/MyNotificationReceiver.kt
@@ -3,12 +3,7 @@ package com.example.pushnotifications
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Context.VIBRATOR_SERVICE
 import android.content.Intent
-import android.os.Build
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.RemoteInput
 
@@ -27,25 +22,6 @@ class MyNotificationReceiver : BroadcastReceiver() {
                 .setContentTitle("Hey Thanks For Reply, ".plus(msg))
             val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(ConstantResource.NOTIFICATION_ID, mBuilder.build())
-            //Methods For Vibrate
-            shakeItVibrator(context)
-        }
-
-        //if help button is clicked
-        if (intent!!.getIntExtra(ConstantResource.KEY_HELP, -1) == ConstantResource.REQUEST_CODE_HELP)
-            Toast.makeText(context, "You Clicked Help", Toast.LENGTH_SHORT).show()
-
-        //if more button is clicked
-        if (intent.getIntExtra(ConstantResource.KEY_MORE, -1) == ConstantResource.REQUEST_CODE_MORE)
-            Toast.makeText(context, "You Clicked More", Toast.LENGTH_SHORT).show()
-    }
-
-    // Vibrate for 150 milliseconds
-    private fun shakeItVibrator(context: Context?) {
-        if (Build.VERSION.SDK_INT >= 26) {
-            (context!!.getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
-        } else {
-            (context!!.getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(150)
         }
     }
 }
